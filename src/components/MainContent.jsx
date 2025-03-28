4bfa18fc955c69c12b46e0f6cb4e38613400568e
manikantay11 <yalamanchilimanikanta005@gmail.com>
2025-03-28 19:44:55 +0530

    first commit

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+import Die from './Die'
+export default function Main() {
+  return (
+    <>
+      <main className="main-container">
+        <h1>Main box</h1>
+        <div className="button-container">
+          <Die />
+        </div>
+      </main>
+    </>
+  )
+}
